7b4bca391a0446dcb1a0f02508646297b825f466
hironobu <ghdtjs0417@gmail.com>
2018-10-10 20:19:51 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p>현재는 베타버전으로 주문/배송 조회만 가능합니다.</p>
+    <p>앞으로 다양한 기능은 물론, cs관리를 위한 솔루션,<p>
+    <p>그리고 챗봇 커스터마이징까지 서비스할 예정입니다.</p>
+    <!-- <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -26,7 +29,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
@@ -41,6 +44,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  margin: 10px 0;
+  font-size: 60px;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +61,9 @@ li {
 }
 a {
   color: #42b983;
+}
+p {
+  font-size: 24px;
+  margin: 0;
 }
 </style>
